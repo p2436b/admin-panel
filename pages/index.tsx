93ebf30279e1534b20e0f21ad8ce49e57494efc8
Peyman Bayat <p2436b@gmail.com>
2023-02-26 21:15:04 +0300

    Add project files

---
+++ pages/index.tsx
@@ -0,0 +1,65 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className='flex gap-2'>
+            <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              NavItem
+            </li>
+            <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              NavItem
+            </li>
+            <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              NavItem
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <hr />
+      <div className='flex'>
+        <aside className='border'>
+          <nav>
+            <ul>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+                NavItem
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main className='border grow p-4'>main</main>
+      </div>
+    </>
+  );
+};
+
+export default Home;
