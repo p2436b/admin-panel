f15ead978fa780fbfe5de8623eee94d009a5430e
Mina Shahamat <maryamshahamat1991@gmail.com>
2023-02-27 23:19:32 +0300

    add table component to main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,65 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+import Table from "../components/table";
 
 const Home: NextPage = () => {
   return (
     <>
       <header>
         <nav>
-          <ul className='flex gap-2'>
-            <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+          <ul className="flex gap-2">
+            <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
               NavItem
             </li>
-            <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+            <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
               NavItem
             </li>
-            <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+            <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
               NavItem
             </li>
           </ul>
         </nav>
       </header>
       <hr />
-      <div className='flex'>
-        <aside className='border'>
+      <div className="flex">
+        <aside className="border">
           <nav>
             <ul>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
-              <li className='px-4 py-2 hover:bg-slate-200 cursor-pointer'>
+              <li className="px-4 py-2 hover:bg-slate-200 cursor-pointer">
                 NavItem
               </li>
             </ul>
           </nav>
         </aside>
-        <main className='border grow p-4'>main</main>
+        <main className="border grow p-4">
+          <Table />
+        </main>
       </div>
     </>
   );
