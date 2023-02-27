7db6a48a48ef74a67fb9d36393e56d208d671390
Mina Shahamat <maryamshahamat1991@gmail.com>
2023-02-27 23:17:58 +0300

    create a table cogitmponent

---
+++ components/table.tsx
@@ -0,0 +1,34 @@
+const Table: React.FC = () => {
+  const data = [
+    { c1: 1, c2: "2", c3: "3", c4: "4" },
+    { c1: "1", c2: "2", c3: "3", c4: "4" },
+    { c1: "1", c2: "2", c3: "3", c4: "4" },
+    { c1: "1", c2: "2", c3: "3", c4: "4" },
+    { c1: "1", c2: "2", c3: "3", c4: "4" },
+    { c1: "1", c2: "2", c3: "3", c4: "4" },
+    { c1: "mina", c2: "2", c3: "3", c4: "4" },
+  ];
+  return (
+    <table className="border-2 w-full h-full text-center">
+      <thead>
+        <tr>
+          <th>A</th>
+          <th>B</th>
+          <th>C</th>
+          <th>D</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr>
+            <td>{row.c1}</td>
+            <td>{row.c2}</td>
+            <td>{row.c3}</td>
+            <td>{row.c4}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+export default Table;
